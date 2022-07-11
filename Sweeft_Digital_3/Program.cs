4d687563c6bb431ca6804c5c6443e9ef1208122f
Nen <Nen@DESKTOP-EPGE2L2>
2022-07-11 20:39:24 +0400

    Add project files.

---
+++ Sweeft_Digital_3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace Sweeft_Digital_3
+{
+    internal class Program
+    {
+        public static int MyArrayMinValue(int[] array)
+        {
+            int minvalue = 1;
+            for (int i = 1; i <= int.MaxValue; i++)
+            {
+                bool found = false;
+                
+                foreach (var item in array)
+                {
+                    if (i == item) {
+                        found = true;
+                        break;                  
+                    }                  
+                }
+
+                if (!found)
+                {
+                    minvalue = i;
+                    break;
+                }               
+            }      
+           
+            return minvalue;
+        }
+        static void Main(string[] args)
+        {
+            int[] MyArray = { 1, 2, 3, 5, 13 };
+            int resultMinValue = MyArrayMinValue(MyArray);
+            Console.WriteLine($"MinValue : {resultMinValue}");
+        }
+    }
+}
+
